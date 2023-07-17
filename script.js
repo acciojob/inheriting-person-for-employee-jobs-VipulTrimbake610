d209b00436d775cecc4533848b0f7474b8d6a20f
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-07-17 11:52:34 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // complete this js code
-function Person(name, age) {}
-Person.prototype.greet = function() {
-	
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
 }
-function Employee(name, age, jobTitle) {}
-
+Person.prototype.greet = function() {
+	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old.");
+}
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+}
+// const person = new Person("Alice", 25);
+// console.log(person);
+// person.greet();
+Employee.prototype.jobGreet(){
+	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle+".");
+}
+// console.log(person.__proto__.greet());
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
